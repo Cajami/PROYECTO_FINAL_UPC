537483356d2bb2e28870c1073b8fc4da7cd76515
Javier Huiñocana <javier2315@gmail.com>
2018-07-08 01:24:06 -0500

    agregando controles al mainuseractivity

--- app/src/main/java/pe/cajami/com/mechanicalassistanceapp/activities/MainClientActivity.java
+++ app/src/main/java/pe/cajami/com/mechanicalassistanceapp/activities/MainClientActivity.java
@@ -2,14 +2,27 @@ package pe.cajami.com.mechanicalassistanceapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import pe.cajami.com.mechanicalassistanceapp.R;
 
 public class MainClientActivity extends AppCompatActivity {
 
+    Button btnEmergenciaPendiente_MainUsuer,
+            btnVerHistorialEmergencias_MainUsuer,
+            btnVerProveedores_MainUsuer,
+            btnEditarUsuario_MainUsuer,
+            btnRegistrarEmergencia_MainUsuer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_client);
+
+        btnEmergenciaPendiente_MainUsuer = (Button) findViewById(R.id.btnEmergenciaPendiente_MainUsuer);
+        btnVerHistorialEmergencias_MainUsuer = (Button) findViewById(R.id.btnVerHistorialEmergencias_MainUsuer);
+        btnVerProveedores_MainUsuer = (Button) findViewById(R.id.btnVerProveedores_MainUsuer);
+        btnEditarUsuario_MainUsuer = (Button) findViewById(R.id.btnEditarUsuario_MainUsuer);
+        btnRegistrarEmergencia_MainUsuer = (Button) findViewById(R.id.btnRegistrarEmergencia_MainUsuer);
     }
 }
